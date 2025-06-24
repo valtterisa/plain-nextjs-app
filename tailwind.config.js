a09451996d2d7e0e9c20e6075c0be9a22ebfec07
Valtteri <savonen.emppu@gmail.com>
2025-06-24 12:56:48 +0300

    make this bitch use npm run dev for hot reload file changes and make it have the whole file system instead of static output

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,32 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   safelist: [
-    {
-      pattern: /.*/,
-    },
+    // Typography
+    { pattern: /^font-(sans|serif|mono|thin|extralight|light|normal|medium|semibold|bold|extrabold|black)$/ },
+    { pattern: /^text-(xs|sm|base|lg|xl|2xl|3xl|4xl|5xl|6xl|7xl|8xl|9xl)$/ },
+    { pattern: /^text-(left|center|right|justify)$/ },
+    { pattern: /^text-(\w+)(-\d{3})?$/ }, // text colors
+    { pattern: /^leading-(none|tight|snug|normal|relaxed|loose|\d+)$/ },
+    { pattern: /^tracking-(tighter|tight|normal|wide|wider|widest)$/ },
+
+    // Margin & Padding
+    { pattern: /^m[trblxy]?-\d+$/ },
+    { pattern: /^p[trblxy]?-\d+$/ },
+
+    // Background colors
+    { pattern: /^bg-(\w+)(-\d{3})?$/ },
+
+    // Border
+    { pattern: /^border(-(t|r|b|l|x|y))?(-\d+)?$/ },
+    { pattern: /^border-(\w+)(-\d{3})?$/ }, // border colors
+    { pattern: /^rounded(-(none|sm|md|lg|xl|2xl|3xl|full))?$/ },
+    { pattern: /^border-(solid|dashed|dotted|double|none)$/ },
+
+    // Opacity
+    { pattern: /^opacity-\d+$/ },
+
+    // Shadow
+    { pattern: /^shadow(-(sm|md|lg|xl|2xl|inner|none))?$/ },
   ],
   theme: {
     extend: {},
