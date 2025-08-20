8e481ceba4b6449be6c51980fe68337b5a498c5b
Valtteri <savonen.emppu@gmail.com>
2025-08-20 12:27:00 +0300

    change the text on page to be better

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <p>This is a test. Fly.io works if you get this text!</p>
+      <p>This is a plain Next.js app. Start building using the chat!</p>
     </div>
   );
 }
